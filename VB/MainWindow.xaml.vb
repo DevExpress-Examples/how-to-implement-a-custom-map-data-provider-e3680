7f71a8e79730fe9d2d3a5d8ed3d652748ed1ac82
DevExpressExampleBot <fakeEmail>
2025-03-14 13:52:59 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,37 +1,38 @@
-﻿Imports System
+Imports System
 Imports System.Globalization
 Imports System.Windows
 Imports DevExpress.Xpf.Map
 
 Namespace CustomMapDataProviderApp
 
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            imageLayer.DataProvider = New CustomMapDataProvider()
+            Me.InitializeComponent()
+            Me.imageLayer.DataProvider = New CustomMapDataProvider()
         End Sub
     End Class
 
     Public Class CustomMapDataProvider
         Inherits MapDataProviderBase
 
+        Private ReadOnly projectionField As SphericalMercatorProjection = New SphericalMercatorProjection()
 
-        Private ReadOnly projection_Renamed As New SphericalMercatorProjection()
-
-        Public Overrides ReadOnly Property Projection() As ProjectionBase
+        Public Overrides ReadOnly Property Projection As ProjectionBase
             Get
-                Return projection_Renamed
+                Return projectionField
             End Get
         End Property
 
         Public Sub New()
             SetTileSource(New CustomTileSource())
         End Sub
+
         Protected Overrides Function CreateObject() As MapDependencyObject
             Return New CustomMapDataProvider()
         End Function
+
         Public Overrides Function GetMapSizeInPixels(ByVal zoomLevel As Double) As Size
             Return New Size(Math.Pow(2.0, zoomLevel) * OpenStreetMapTileSource.tileSize, Math.Pow(2.0, zoomLevel) * OpenStreetMapTileSource.tileSize)
         End Function
@@ -40,18 +41,22 @@ Namespace CustomMapDataProviderApp
     Public Class CustomTileSource
         Inherits MapTileSourceBase
 
-        Private Const roadUrlTemplate As String = "http://{subdomain}.tile.openstreetmap.org/{tileLevel}/{tileX}/{tileY}.png"
+        Const roadUrlTemplate As String = "http://{subdomain}.tile.openstreetmap.org/{tileLevel}/{tileX}/{tileY}.png"
+
         Public Const maxZoomLevel As Integer = 20
+
         Public Const tileSize As Integer = 256
 
-        Private Shared imageWidth As Integer = CInt((Math.Pow(2.0, maxZoomLevel))) * tileSize
-        Private Shared imageHeight As Integer = CInt((Math.Pow(2.0, maxZoomLevel))) * tileSize
-        Private Shared subdomains() As String = { "a", "b", "c" }
+        Private Shared imageWidth As Integer = CInt(Math.Pow(2.0, maxZoomLevel)) * tileSize
+
+        Private Shared imageHeight As Integer = CInt(Math.Pow(2.0, maxZoomLevel)) * tileSize
+
+        Private Shared subdomains As String() = New String() {"a", "b", "c"}
 
         Public Sub New()
             MyBase.New(imageWidth, imageHeight, tileSize, tileSize)
-
         End Sub
+
         Public Overrides Function GetTileByZoomLevel(ByVal zoomLevel As Integer, ByVal tilePositionX As Long, ByVal tilePositionY As Long) As Uri
             Dim url As String = roadUrlTemplate
             url = url.Replace("{tileX}", tilePositionX.ToString(CultureInfo.InvariantCulture))
